210bb12747f1665ba4462b31b200f0c928f6efd1
Elias Axelsson <elias.axelsson@ericsson.com>
2024-12-03 12:53:03 +0100

    day4 setup

---
+++ 2024/day4/main.py
@@ -0,0 +1,20 @@
+def main():
+    inputFile = open("TestInput.txt", "r")
+    lines = inputFile.read().splitlines()
+
+    part1(lines)
+    part2(lines)
+
+
+def part1(lines):
+    sum = 0
+    print(sum)
+
+
+def part2(lines):
+    sum = 0
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
